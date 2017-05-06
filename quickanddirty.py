817ad9a93bb2fd51bd52e5930fe194f40b005e1e
Caleb Tenberge <Caleb@cten-mbp.local>
2017-05-06 01:50:02 -0400

    Fixed the curl command in the user_data

--- quickanddirty.py
+++ quickanddirty.py
@@ -11,6 +11,12 @@ keypair = client.create_key_pair(KeyName='challengekey')
 keyfile.write(str(keypair['KeyMaterial']))
 keyfile.close()
 
+user_data = '''
+#!/bin/bash
+curl -L -o main https://github.com/cten/quick-and-dirty/raw/master/main
+chmod 755 main
+./main
+'''
 
 vpc = ec2.create_vpc(CidrBlock='10.0.0.0/24')
 for s in vpc.security_groups.all():
@@ -25,7 +31,7 @@ sg.authorize_ingress(FromPort=22,ToPort=22,CidrIp='0.0.0.0/0',IpProtocol='TCP')
 sg.authorize_ingress(FromPort=80,ToPort=80,CidrIp='0.0.0.0/0',IpProtocol='TCP')
 sg.authorize_ingress(FromPort=8080,ToPort=8080,CidrIp='0.0.0.0/0',IpProtocol='TCP')
 
-instances = ec2.create_instances(ImageId='ami-0099bd67', KeyName="challengekey", MinCount=1, MaxCount=1, SubnetId=subnet.id ,InstanceType="t2.micro", UserData='#!/bin/bash\ncurl ')
+instances = ec2.create_instances(ImageId='ami-0099bd67', KeyName="challengekey", MinCount=1, MaxCount=1, SubnetId=subnet.id ,InstanceType="t2.micro", UserData=user_data)
 
 gateway.attach_to_vpc(VpcId=vpc.id)
 route_table.create_route(GatewayId=gateway.id, DestinationCidrBlock='0.0.0.0/0')
